a1ac43688ebbbf6230887381e3f77fd7d6c8a2d2
Valerii Kuznetsov <valerii_kuznetsov@epam.com>
2021-05-30 21:42:12 +0300

    practic-3

---
+++ practic_3/app/src/main/java/ru/rsue/kuznetsov/MainActivity.kt
@@ -0,0 +1,28 @@
+package ru.rsue.kuznetsov
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.Toast
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var mTrueButton: Button
+    private lateinit var mFalseButton: Button
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        mTrueButton = findViewById(R.id.true_button)
+        mTrueButton.setOnClickListener {
+            Toast.makeText(this, R.string.correct_toast,
+                Toast.LENGTH_SHORT).show()
+        }
+        mFalseButton = findViewById(R.id.false_button)
+        mFalseButton.setOnClickListener{
+            Toast.makeText(this, R.string.incorrect_toast,
+                Toast.LENGTH_SHORT).show()
+        }
+    }
+}
